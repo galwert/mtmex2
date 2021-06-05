f78e33019659825a7d4ac29fa9c1ab71481f8a10
gal <wertheizer@campus.technion.ac.il>
2021-06-05 22:09:00 +0300

    added sortedlist class

---
+++ SortedList.cpp
@@ -0,0 +1,4 @@
+//
+// Created by galwe on 05/06/2021.
+//
+
---
+++ SortedList.h
@@ -0,0 +1,8 @@
+//
+// Created by galwe on 05/06/2021.
+//
+
+#ifndef HW2_SORTEDLIST_H
+#define HW2_SORTEDLIST_H
+
+#endif //HW2_SORTEDLIST_H
