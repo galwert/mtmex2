ac0e798388f26851debed8b009aad853cb80a5bd
gal <wertheizer@campus.technion.ac.il>
2021-06-05 15:54:48 +0300

    added exmadetails.h

---
+++ ExamDetails.h
@@ -0,0 +1,14 @@
+//
+// Created by galwe on 05/06/2021.
+//
+
+#ifndef HW2_EXAMDETAILS_H
+#define HW2_EXAMDETAILS_H
+class ExamDetails
+{
+private:
+    int course_number;
+    int test_month;
+    int test_day;
+};
+#endif //HW2_EXAMDETAILS_H
