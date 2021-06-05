fb77d555fed9121a704dc607c7d95ff2fa9d1f08
gal <wertheizer@campus.technion.ac.il>
2021-06-05 16:08:04 +0300

    update

--- ExamDetails.h
+++ ExamDetails.h
@@ -1,14 +1,19 @@
 //
 // Created by galwe on 05/06/2021.
-//
-
+using namespace std::string;
 #ifndef HW2_EXAMDETAILS_H
 #define HW2_EXAMDETAILS_H
+
+
 class ExamDetails
 {
 private:
     int course_number;
     int test_month;
     int test_day;
+    int test_hour;
+    enum minute{0,30} test_minute;
+    int length;
+    string link;
 };
 #endif //HW2_EXAMDETAILS_H
