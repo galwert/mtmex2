b79e616d0d916b5c0a0d476d69c92462528d567e
= <or.twito05@gmail.com>
2021-06-08 16:17:07 +0300

    08/06 16:16

--- SortedList.h
+++ SortedList.h
@@ -3,31 +3,77 @@
 
 #include <iostream>
 #include <string>
+#include "stdlib.h"
 
 template <class T>
 
 class SortedList
 {
-    typedef struct node_t
+    typedef class node_t
     {
-        T data;
-        struct node_t *next;
+        T data = NULL;
+        class node_t *next = NULL;
     } *Node;
 
     class const_iterator
     {
-        // iterator part
+    private:
+        Node current_node;
+
+        const_iterator()
+        {
+            Node current_node = head;
+        }
+
+    public:
+        const_iterator(const SortedList<T>::const_iterator &iterator_to_copy)
+        {
+            iterator.current_node = iterator_to_copy.current_node;
+        }
+
+        ~const_iterator()
+        {
+            return;
+        }
+
+        const_iterator& const_iterator::operator=(const const_iterator() &iter_to_assign)
+        {
+            iterator.current_node = iterator_to_assign.current_node;
+            return *this;
+        }
+
+        void operator++ ()
+        {
+            if(iterator.current_node->next == NULL)
+            {
+                throw std::out_of_range;
+            }
+            iterator.current_node = iterator.current_node->next;
+        }
+
+        bool operator== (const const_iterator& iterator1) const
+        {
+            return iterator.current_node == iterator1.current_node;
+        }
+
+        const const_iterator& SortedList<T>::const_iterator::operator* ()
+        {
+            SortedList<T>::const_iterator iterator_data = *iterator.current_node;
+            return iterator_data;
+        }
     };
 
 private:
     Node head;
     int size;
+    const_iterator iterator;
 
 public:
     SortedList()
     {
         head = NULL;
         size = 0;
+        iterator = head;
     }
 
     ~SortedList()
@@ -82,7 +128,7 @@ public:
         new_node->next = NULL;
     }
 
-    void insert(T new_data)
+    void insert(T new_data) const
     {
         Node runner = head;
         while(runner->next->data < new_data)
@@ -99,7 +145,7 @@ public:
         size = size + 1;
     }
 
-    void remove(T iterator)
+    void remove(T iterator) const
     {
         if(iterator == head->data)
         {
@@ -119,6 +165,81 @@ public:
     int length()
     {
         return size;
+    }
+
+    template <class Condition>
+    SortedList<T>& filter(Condition condition) const
+    {
+        Node current_node = head;
+        SortedList<T> new_list = new SortedList<T>();
+        if(size == 0)
+        {
+            return new_list;
+        }
+        Node new_node = NULL;
+        Node next_node = NULL;
+        new_list.head = new_node;
+        while(current_node != NULL)
+        {
+            if (condition(current_node->data))
+            {
+                new_node = new Node();
+                new_node->data = current_node->data;
+                next_node = new Node();
+                new_node->next = next_node;
+            }
+            current_node = current_node->next;
+            new_node = new_node->next;
+        } // List is already sorted so new list has to be sorted too
+
+        if(next_node != NULL)
+        {
+            delete(next_node);
+        }
+
+        return new_list;
+    }
+
+    template <class Function>
+    SortedList<T>& apply(SortedList<T> Function) const
+    {
+        Node current_node = head;
+        SortedList<T> new_list = SortedList<T>();
+        if(size == 0)
+        {
+            return new_list;
+        }
+
+        Node new_node = new Node();
+        new_list.head = new_node;
+
+        while(current_node != NULL)
+        {
+            new_node->data = Function(current_node);
+            Node next_node = new Node();
+            new_node->next = next_node;
+            current_node = current_node->next;
+            next_node = new_node->next;
+            if(current_node == NULL)
+            {
+                delete(next_node);
+            }
+        }
+        return new_list;
+    }
+
+    void begin(const_iterator iterator)
+    {
+        iterator.current_node = head;
+    }
+
+    void end(const_iterator iterator)
+    {
+        iterator = head;
+        while(iterator.current_node->next != NULL)
+        {
+            iterator.current_node = iterator.current_node->next;
+        }
     }
 };
 
